80bb0e0c29497e82a0e5e3e00612e5df61d14c10
Priyanshu Kumar <41790340+priyanshu-kumar02@users.noreply.github.com>
2020-03-22 03:27:33 +0530

    chore: fermat

--- MMI._Modulo.cpp
+++ MMI._Modulo.cpp
@@ -1,3 +1,20 @@
+//CODE TO FIND MMI USING FERMET THEOREM.
+//MMI(y,M)=[y^(m-2)]%M;
+
+ll pow(ll x, ll y,ll m) {
+    if( y == 0) return 1;
+    ll half = pow(x, y/2, m);
+    ll full = (half * half) % m;
+    return y%2 ? (x * full) % m : full;
+}
+
+int findMMI_fermat(int n,int M) {
+    return pow(n,M-2,M);
+}
+
+//NOTES:
+//source:https://codeaccepted.wordpress.com/2014/02/15/output-the-answer-modulo-109-7/
+
 1. ( a + b ) % c = ( ( a % c ) + ( b % c ) ) % c
 2. ( a * b ) % c = ( ( a % c ) * ( b % c ) ) % c
 3. ( a – b ) % c = ( ( a % c ) – ( b % c ) ) % c ( see note )
@@ -16,19 +33,4 @@ if M=13, the MMI of 7 is 2 as ( 7 * 2 ) % 13==1.
 
 Now, (x/y)%M = x*MMI(y,M);
 
-CODE TO FIND MMI USING FERMET THEOREM.
-MMI(y,M)=[y^(m-2)]%M;
 
-ll pow(ll x, ll y,ll m) {
-    if( y == 0) return 1;
-    ll half = pow(x, y/2, m);
-    ll full = (half * half) % m;
-    return y%2 ? (x * full) % m : full;
-}
-
-int findMMI_fermat(int n,int M) 
-{
-    return pow(n,M-2,M);
-}
-
-source:https://codeaccepted.wordpress.com/2014/02/15/output-the-answer-modulo-109-7/
