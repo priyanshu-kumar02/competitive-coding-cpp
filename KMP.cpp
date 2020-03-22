2fc058d3986d6ef7ee298789436082d5c681f035
Priyanshu Kumar <41790340+priyanshu-kumar02@users.noreply.github.com>
2020-03-22 22:48:51 +0530

    update KMP

--- KMP.cpp
+++ KMP.cpp
@@ -1,5 +1,3 @@
-//0-based indexing
-
 vector<int> computeLPS(string pat) {					
     vector<int> lps(pat.size(), 0);
     int len = 0;
@@ -15,31 +13,24 @@ vector<int> computeLPS(string pat) {
     return lps;
 }
 
-void kmp(string pat,string text)
+//seen return all indexes where pat starts (0-bases)
+vector<int> kmp(string text, string pat)
 {
-	ll n=text.size();
-	ll m=pat.size();
-	int i=0,j=0;
-	vector<int> lps = computeLPS(pat);
+    vector<int> seen_at;
+    int len = 0;
+    vector<int> lps = computeLPS(pat);
 
-	while (i<n)
-	{
-		if (text[i]==pat[j])
-		i++,j++;
-		
-		if(j==m)
-		{
-		printf("%lld\n",i-j);
-		j=lps[j-1];
-		}
-		
-		else if(i<n and pat[j]!=text[i])
-		{
-			if(j!=0)
-			j=lps[j-1];
-			else
-			i++;
-		}
-	}
-	
+    for (int i = 0; i < text.size(); i++) {
+        while (text[i] != pat[len] and len != 0)
+            len = lps[len - 1];
+
+        if (text[i] == pat[len])
+            len++;
+
+        if (len == pat.size()){
+            seen_at.push_back(i - len + 1);
+            len = lps[len - 1];
+        }
+    }
+    return seen_at;
 }
