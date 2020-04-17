8a3220c85bb5917f76b7a2a4e856b3f724a25e30
Priyanshu Kumar <41790340+priyanshu-kumar02@users.noreply.github.com>
2020-04-17 23:59:31 +0530

    chore: update DSU

--- DSU.cpp
+++ DSU.cpp
@@ -1,49 +1,13 @@
-//UNION-FIND DATA STRUCTURE -- COMPRESSED FIND ROOT O(LOG* N)
-void initialise(lli arr[],lli size[],lli n)
-{
-	for (lli i=0 ; i<n ; i++)
-	{
-		arr[i]=i;
-		size[i]=1;
-	}
-	
+vector<int> dsu(SZ);
+
+int root(int x) {
+    return (x==dsu[x]) ? x : dsu[x] = root(dsu[x]);
 }
 
-lli root(lli arr[],lli i)
-{
-	while (i!=arr[i])
-	{
-		arr[i]=arr[arr[i]];
-		i=arr[i];
-	}
-	
-return i;
-
+void connect(int u, int v) {
+    dsu[root(u)] = root(v);
 }
 
-lli find(lli arr[0],lli A,lli B)
-{
-return root(arr,A)==root(arr,B) ? 1 : 0 ;
-}
-
-void unite_weighted(lli arr[],lli size[],lli A,lli B)
-{
-	
-	lli root_A=root(arr,A);
-	lli root_B=root(arr,B);
-	
-	if(root_A!=root_B)
-	{
-		if(size[root_A]<size[root_B])
-		{
-		   arr[root_A]=root_B;
-		   size[root_B]+=size[root_A];
-		}
-
-		else
-		{
-		   arr[root_B]=root_A;
-		   size[root_A]+=size[root_B];	
-		}
-	}
+void initialise() {
+   for(int i=1; i<=n; i++) dsu[i]=i;
 }
