26fc224e310fb6287eabf6aabad59f205b5dfa1c
Priyanshu Kumar <41790340+priyanshu-kumar02@users.noreply.github.com>
2020-05-02 18:57:30 +0530

    update topo_sort

---
+++ Graph/topo_sort_DAG_KAHNS_ALGO.cpp
@@ -0,0 +1,88 @@
+//DFS-based
+
+int n; // number of vertices
+vector<vector<int>> adj; // adjacency list of graph
+vector<bool> vis(SZ, false);
+vector<int> topo;
+
+void dfs(int v) {
+    vis[v] = true;
+    for (int u : adj[v]) {
+        if (!vis[u])
+            dfs(u);
+    }
+    topo.push_back(v);
+}
+
+void topological_sort() {
+    for (int i = 0; i < n; ++i) {
+        if (!vis[i])
+            dfs(i);
+    }
+    reverse(all(topo));
+}
+
+//BFS-based
+
+/****   KAHN's ALGORITHM toposort using BFS.     ****/
+// Problem: lexicographically smallest topological sort https://www.spoj.com/problems/TOPOSORT/
+// detects cycle also (no toposort exists)
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define vec vector
+#define pb push_back
+#define s(x) scanf("%d",&x)
+#define all(x)	x.begin(),x.end()
+#define pii pair<int,int>
+#define fi first
+#define se second
+#define SZ 100005
+#define boost ios::sync_with_stdio(0);
+
+vec<int> adj[100005];
+int in[100005]; //stores indegree of each vertex
+vec<int> topo;
+int vis_cnt = 0;
+priority_queue <int, vector<int>, greater<int>> q; //min-heap
+
+void bfs_topo()
+{
+	while (!q.empty()) {
+		int u=q.top();
+		q.pop();
+		topo.pb(u);
+        vis_cnt++;
+		
+		for(int v:adj[u]) {
+			in[v]--;
+			if(in[v]==0)
+			q.push(v);
+		} 
+	}
+}
+
+int main()
+{
+	int n,m,u,v;
+    cin >> n >> m;
+
+    for(int i=0; i<m; i++){
+        cin >> u >> v;
+        adj[u].pb(v);
+		in[v]++;
+	}
+
+    for (int i = 1; i <= n; i++){
+        if(in[i] == 0)
+            q.push(i);
+    }
+    bfs_topo();
+	if(vis_cnt < n) {
+        cout << "Sandro fails.";
+        return 0;
+    }  
+	for(int i=0; i<topo.size(); i++)
+        cout << topo[i] << " ";
+}
