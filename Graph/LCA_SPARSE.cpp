13293ac21c008f7af518146ea323b664ee1b8127
Priyanshu Kumar <41790340+priyanshu-kumar02@users.noreply.github.com>
2020-05-27 18:47:37 +0530

    Update LCA_SPARSE.cpp

--- Graph/LCA_SPARSE.cpp
+++ Graph/LCA_SPARSE.cpp
@@ -1,56 +1,45 @@
-int jump[20][1005];
-vec<int> graph[1005];
- 
-int  P[1005],L[1005]; //P=parent,L=level
-int N;
+int L[SZ]; //P=parent,L=level
+vec<int> adj[SZ]; 
+int jump[21][SZ];
  
 //calculates (parent,level) arrays
-void calc(int n) 				 
+void dfs(int u, int p, int l) 
 {
-	if(n==1)
-	L[n]=0,P[n]=-1;
+    jump[0][u] = p;
+    L[u] = l;
 	
-	else
-	L[n] = L[P[n]]+1;
-	
-	for (int i:graph[n])
-	{
-		if(i!=P[n])
-		{
-		 P[i]=n;
-		 calc(i);
-	    }
-	}
+	for (int v: adj[u]) 
+		if(v != p) 
+		dfs(v, u, l+1);
 }
  
 void preprocess_LCA()
-{
-	int i,j;
-	
-	for(j=0;j<=N;j++)
-		jump[0][j]=P[j];
+{	
+	dfs(1, -1, 0);
 		
-	for (i = 1; 1<<i <=N ; i++) 
-		for (j = 0; j <=N ; j++)
-		jump[i][j]= jump[i-1][jump[i-1][j]];
+	for (int i = 1; 1<<i <= n ; i++) 
+	for (int j = 0; j <= n ; j++)
+	jump[i][j] = jump[i-1][jump[i-1][j]];
 }
  
 int LCA(int p,int q)
 {
-	int i,diff;
-	if(L[p]<L[q])
-	swap(p,q);
+	if(L[p] < L[q])
+	swap(p, q);
 	
-	diff=L[p]-L[q];
+	int diff = L[p] - L[q];
 	
-	for(i=12;i>=0;i--) 
-	if( diff&(1<<i)) p=jump[i][p];
+	for(int i = 20; i >= 0; i--) 
+		if(diff & (1<<i)) 
+		p = jump[i][p];
 		
-	if(p==q) return p;
-	 
-	for(i=12;i>=0;i--)
-	 if( jump[i][p]!=jump[i][q] )
-	  p=jump[i][p],q=jump[i][q];
-	
-	return P[p];
+	if(p == q) return p;
+
+	for (int i = 20; i >= 0; i--) {
+		if (jump[i][p] != jump[i][q]) {
+			p = jump[i][p];
+			q = jump[i][q];
+		}
+	}
+	return jump[0][p];
 }
