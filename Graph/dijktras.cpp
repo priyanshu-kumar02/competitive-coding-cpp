454c9f1f255d81f337a9f827326687185379e030
Priyanshu Kumar <41790340+priyanshu-kumar02@users.noreply.github.com>
2020-05-28 17:10:10 +0530

    dijktras

--- Graph/dijktras.cpp
+++ Graph/dijktras.cpp
@@ -1,48 +1,24 @@
-////////////////- src to dest  -//////////////////////////
-//priority queue is faster than set
+vec<pii> adj[SZ];
+int previous[SZ];
 
-vec<pii> adj[10005];
-int V,E;
-
-int dijktra(int x,int y){
-	vec<int> d(V+1,INT_MAX);
-	d[x]=0;
-	priority_queue<pii> cur;
-	cur.push({0,x});
-	
-	while (!cur.empty())
-	{	
-	    int closest=cur.top().se;cur.pop();
-	    if(closest==y) return d[y];
-	    for(auto i: adj[closest])
-			if(d[i.se]> d[closest]+i.fi)
-			{
-			 d[i.se]= d[closest]+ i.fi;
-			 cur.push ({-d[i.se],i.se});
-			}	
-	}
-	return -1;
-}
-
-////////////////////- src to all - ////////////////////////
-int n=100005;
 vector<int> dijktra(int x) {
-	vec<int> d(n+1,INT_MAX);
-        d[x]=0;
-	priority_queue<pii> cur;
-	cur.push({0,x});
+	vec<int> d(SZ + 1, INT_MAX);
+	d[x] = 0;
+	priority_queue< pii, vec<pii>, greater<pii> > pq;
+	pq.push({0, x});
 	
-	while (!cur.empty()) {	
-		int closest=cur.top().se;cur.pop();
-	    	for(auto i: adj[closest])
-			if(d[i.se]> d[closest]+i.fi){
-			 d[i.se]= d[closest]+ i.fi;
-			 cur.push ({-d[i.se],i.se});
+	while (!pq.empty()) {	
+		int closest = pq.top().se;
+		pq.pop();
+		for(auto v: adj[closest]) {
+			int to = v.se;
+			int weight = v.fi;
+			if(d[to] > d[closest] + weight) {
+				d[to] = d[closest] + weight;
+				previous[to] = closest;
+				pq.push({d[to], to});
 			}	
-	}
+		}
+	}	
 	return d;
 }
-
-// amortised complexity of inner for = ElogV
-//amortised complexity of erase operation in outer while= VlogV
-//  => O( (E+V) logV) =>O(ElogV)
