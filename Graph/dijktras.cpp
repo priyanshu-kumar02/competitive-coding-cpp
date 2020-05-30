ed7d674694e50804b08f6883e0b7e9bd277937dd
Priyanshu Kumar <41790340+priyanshu-kumar02@users.noreply.github.com>
2020-05-30 20:40:45 +0530

    dijktras in matrix

--- Graph/dijktras.cpp
+++ Graph/dijktras.cpp
@@ -22,3 +22,30 @@ vector<int> dijktra(int x) {
 	}	
 	return d;
 }
+
+/* Dijktra on MATRIX 
+https://www.hackerrank.com/contests/hack-the-interview-iv-apac/challenges/optimal-path-1/problem */
+
+int dijktra(pii x) {
+    map<pii, int> d;
+    d[x] = 0;
+    priority_queue< pipii, vec<pipii>, greater<pipii> > pq;
+    pq.push({0, {0, 0}});
+    
+    while (!pq.empty()) {    
+        pii closest = pq.top().se;
+        int i = closest.fi;
+        int j = closest.se;
+        pq.pop();
+        for(int k=0; k<4; k++) {
+            int ii = i + di[k];
+            int jj = j + dj[k];
+            
+            if(valid(ii,jj) and  (d.find({ii,jj})==d.end() ||  d[{ii,jj}] > max(d[{i,j}], cost(i,j,ii,jj)))) {
+                d[{ii,jj}]  =  max(d[{i,j}], cost(i,j,ii,jj));
+                pq.push({d[{ii,jj}], {ii,jj}});
+            }   
+        }
+    }    
+    return d[{n-1,m-1}];
+}
