d35a9bdfacc57fa013006def5d5343663994d7d5
Priyanshu Kumar <41790340+priyanshu-kumar02@users.noreply.github.com>
2020-09-06 23:21:07 +0530

    upd diameter BFS

--- Graph/Diameter TREE BFS.cpp
+++ Graph/Diameter TREE BFS.cpp
@@ -1,60 +1,29 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define lli long long int
-#define vec vector
-#define pb push_back
-#define s(x) scanf("%d",&x)
-#define pii pair<int,int>
-#define PI 3.1415926535897932384626
- 
-vec<int> graph[10005];
-pii bfs(int n)
-{
-	vec<int> visited(10005,0);
-	vec<int> dist(10005,0);
-	dist[n]=0;
-	
+vec<int> adj[SZ];
+
+pii bfs(int u) {
+	vec<bool> visited(n+1, false);
+	vec<int> d(n+1,0);
+	d[u] = 0;
 	queue<int> Q;
-	Q.push(n);
-	visited[n]=1;
-	int x;
+	Q.push(u);
+	visited[u] = true;
+	int front;
 	
-	while (!Q.empty())
-	{
-		x=Q.front();
-		Q.pop();
-		visited[x]=1;
-		
-		for (int i:graph[x])
-		{
-			if (!visited[i])
-			{
-				Q.push(i);
-				dist[i]=dist[x]+1;
+	while (!Q.empty()) {
+		front = Q.front(); Q.pop();
+		visited[front] = true;
+		for (int v: adj[front]) {
+			if (!visited[v]) {
+				Q.push(v);
+				d[v] = d[front]+1;
 			}
-			
 		}
-		
 	}
-	//cout<<x<<","<<dist[x]<<endl;
-	return {x,dist[x]};
+	return { front, d[front]};
 }
- 
-int main()
-{
-	//freopen("input.txt","r",stdin);
-	int v,x,y;
-	cin>>v;
-	for (int i = 0; i < v-1; i++)
-	{
-		cin>>x>>y;
-		graph[x].pb(y);
-		graph[y].pb(x);
-	}
-	pii far=bfs(1);
-	pii farthest=bfs(far.first);
-	cout<<farthest.second;
- 
- 
+
+int diameter() {
+	pii far = bfs(1);
+	pii farthest = bfs(far.first);
+	return farthest.se;
 }
- 
