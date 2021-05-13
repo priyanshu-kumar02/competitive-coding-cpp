f166d917450248074e0400395213ad5e5c05c7cb
Priyanshu Kumar <41790340+priyanshu-kumar02@users.noreply.github.com>
2021-05-14 03:27:31 +0530

    update quicksiort

--- quick SORT.cpp
+++ quick SORT.cpp
@@ -1,23 +1,12 @@
-lli partition(lli arr[],lli start,lli end)
-{
-	lli j=start+1;
-	lli pivot=arr[start];
-	
-	for(lli i=start+1;i<=end;i++)
-	{
-		if(arr[i]<pivot)
-		swap(arr[i],arr[j++]);
-	}
-	swap(arr[start],arr[j-1]);
-	return j-1;
-}
-
-void quicksort(lli arr[],lli start,lli end)
-{
-	if(start<end)
-	{
-		int piv_pos=partition(arr,start,end);
-		quicksort(arr,start,piv_pos-1);
-		quicksort(arr,piv_pos+1,end);
-	}
-}
+    void quickSort(int ss, int se, vector<int>& nums) {
+        if(ss>=se) return;
+        int idx=ss+1;
+        
+        for(int i=ss+1; i<=se; i++) 
+            if(nums[i]<nums[ss]) 
+                swap(nums[i], nums[idx++]);
+        idx--;
+        swap(nums[ss], nums[idx]);
+        quickSort(ss, idx-1, nums);
+        quickSort(idx+1, se, nums);
+    }
